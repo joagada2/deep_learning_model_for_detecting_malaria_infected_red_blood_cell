77d5bedd5e0b84c2eccae5e0d076d96dcff5c9a5
joagada2 <digitalmarketplaceng@gmail.com>
2023-07-13 08:07:47 -0700

    served model with fastAPI

---
+++ app/preprocess.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
+from tensorflow.keras.models import load_model
+
+model = None
+
+def get_model():
+    model = load_model('model/malaria_detector_model.h5')
+    print("Model loaded")
+    return model
+
+def predict(image: Image.Image):
+    global model
+    if model is None:
+        model = get_model()
+
+    image = np.asarray(image.resize((130, 130)))[..., :3]
+    image = np.expand_dims(image, axis=0)
+    #image = image / 127.5 - 1.0
+
+    result =  ((model.predict(image) > 0.5) * 1).tolist()
+
+    return result
+
+
+def read_imagefile(file) -> Image.Image:
+    image = Image.open(BytesIO(file))
+    return image
--- src/training_flow.py
+++ src/training_flow.py
@@ -1,32 +1,25 @@
-import os
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.image import imread
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
-import warnings
-from sklearn.model_selection import train_test_split
 from datetime import datetime, timedelta
 from prefect import Flow, Parameter, task
 from prefect.schedules import IntervalSchedule
-from sklearn.metrics import classification_report,confusion_matrix
 import warnings
-
 warnings.filterwarnings(action="ignore")
-import numpy as np
-import xgboost as xgb
-import joblib
-import pandas as pd
 from sklearn.metrics import balanced_accuracy_score, f1_score, \
     roc_auc_score, precision_score, recall_score
-from xgboost import XGBClassifier
 import os
 import mlflow
 from dagshub import DAGsHubLogger
+from tensorflow.keras.models import load_model
+
+image_shape = (130,130,3)
+
+train_path = 'data\cell_images/train'
+test_path = 'data\cell_images/test'
+
+batch_size = 16
 
 image_gen = ImageDataGenerator(rotation_range=20,  # rotate the image 20 degrees
                                width_shift_range=0.10,  # Shift the pic width by a max of 5%
@@ -50,8 +43,8 @@ test_image_gen = image_gen.flow_from_directory(test_path,
                                                batch_size=batch_size,
                                                class_mode='binary', shuffle=False)
 
-train_path = 'data\cell_images/train'
-test_path = 'data\cell_images/test'
+train_path = 'data\cell_images\train'
+test_path = 'data\cell_images\test'
 
 @task
 def train_model():
@@ -87,9 +80,6 @@ def train_model():
 
     early_stop = EarlyStopping(monitor='val_loss', patience=2)
 
-    batch_size = 16
-
-
     warnings.filterwarnings('ignore')
 
     model.fit_generator(train_image_gen, epochs=20,
@@ -101,14 +91,14 @@ def train_model():
 @task
 def evaluate(a):
     #link up to dagshub MLFlow environment
-    os.environ['MLFLOW_TRACKING_URI'] = ''
-    os.environ['MLFLOW_TRACKING_USERNAME'] = ''
-    os.environ['MLFLOW_TRACKING_PASSWORD'] = ''
+    os.environ['MLFLOW_TRACKING_URI'] = 'https://dagshub.com/joe88data/deep_learning_model_for_detecting_malaria_infected_red_blood_cell.mlflow'
+    os.environ['MLFLOW_TRACKING_USERNAME'] = 'joe88data'
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = 'e94114ca328c75772401898d749decb6dbcbeb21'
     with mlflow.start_run():
         # Load data and model
-
+        a  = load_model('model/malaria_detector_model.h5')
         a.evaluate_generator(test_image_gen)
-        pred_probabilities = model.predict_generator(test_image_gen)
+        pred_probabilities = a.predict_generator(test_image_gen)
 
         # Get predictions
         predictions = pred_probabilities > 0.5
@@ -139,39 +129,22 @@ def evaluate(a):
                 mlflow.log_metrics(metrics)
                 self.logger.log_metrics(metrics)
 
-            def log_params(self, params: dict):
-                mlflow.log_params(params)
-                self.logger.log_hyperparams(params)
         logger = BaseLogger()
-        # function to log parameters to dagshub and mlflow
-        def log_params(a: Sequential):
-            logger.log_params({"model_class": type(c).__name__})
-            model_params = a.get_params()
-
-            for arg, value in model_params.items():
-                logger.log_params({arg: value})
 
         # function to log metrics to dagshub and mlflow
         def log_metrics(**metrics: dict):
             logger.log_metrics(metrics)
         # log metrics to remote server (dagshub)
-        log_params(a)
+        #log_params(a)
         log_metrics(f1_score=f1, accuracy_score=accuracy, area_Under_ROC=area_under_roc, precision=precision,
                 recall=recall)
-            # log metrics to local mlflow
-            # mlflow.sklearn.log_model(model, "model")
-            # mlflow.log_metric('f1_score', f1)
-            # mlflow.log_metric('accuracy_score', accuracy)
-            # mlflow.log_metric('area_under_roc', area_under_roc)
-            # mlflow.log_metric('precision', precision)
-            # mlflow.log_metric('recall', recall)
 
 #adding schedule here automate the pipeline and make it run every 10 minutes
 schedule = IntervalSchedule(interval=timedelta(minutes=10))
 
 #create and run flow locally. To schedule to workflow to be automatically triggered every 4 hrs,
 #add 'schedule' as Flow parameter (ie with Flow("loan-default-prediction", schedule)
-with Flow("malaria_detection_model_training_flow", schedule) as flow:
+with Flow("malaria_detection_model_training_flow") as flow:
     model = train_model()
     evaluate(model)
 
